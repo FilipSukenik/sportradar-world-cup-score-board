7c4907fe66ce1418c9167849400a294b72241a0c
filip.sukenik <filip.sukenik@accenture.com>
2022-06-29 12:30:02 +0200

    Add javadoc to comparator

--- world-cup-score-board/src/main/java/com/sportradar/sukenik/world/cup/score/board/comaparator/TotalScoreDescendingTimestampAscendingGameComparator.java
+++ world-cup-score-board/src/main/java/com/sportradar/sukenik/world/cup/score/board/comaparator/TotalScoreDescendingTimestampAscendingGameComparator.java
@@ -9,8 +9,25 @@ import java.util.Comparator;
 
 import com.sportradar.sukenik.world.cup.score.board.db.model.GameEntity;
 
+/**
+ * Comparator that contains comparison logic for {@link com.sportradar.sukenik.world.cup.score.board.service.ScoreBoardService}.
+ */
 public class TotalScoreDescendingTimestampAscendingGameComparator implements Comparator<GameEntity> {
 
+    /**
+     * In case that both passed objects are null returns 0 meaning objects are same. In case that one of objects is null
+     * returns that not null object should go before the null. Next parameter to compare is total score, in this case returns
+     * subtract of scores.
+     *
+     * Next parameter is to compare creation timestamps - the list should be ordered that the latest added should be before older
+     * records (in case that score is equal).
+     *
+     * If both records have same creation timestamp method {@link GameEntity#getGameId()} decides which record was added later.
+     * @param o1 first object to compare.
+     * @param o2 second object to compare.
+     * @return comparison of 2 nullable object based on required business logic - first compare total score, then creation timestamp
+     * and then gameId.
+     */
     @Override
     public int compare(GameEntity o1, GameEntity o2) {
 
